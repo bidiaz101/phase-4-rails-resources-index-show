395b8ed0e36529cab1a07b79b79d7d170f66b4f7
Brian Diaz <bidiaz101@gmail.com>
2021-11-18 14:59:30 -0600

    commit tree 1b07da78ef3d2d352884f799b57d5839b9209395

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,18 @@
+class BirdsController < ApplicationController
+
+    def index
+        birds = Bird.all
+        render json: birds
+    end
+
+    def show
+        bird = Bird.find_by(id: params[:id])
+
+        if bird
+            render json: bird
+        else
+            render json: { error: "Bird not found" }, status: 404
+        end
+    end
+
+end
